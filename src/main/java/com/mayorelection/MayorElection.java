1c7d7fd7ac4612475eb09704c3d7de5b91c04fd5
Md Mahfuz Rahman <tanzimrani8@gmail.com>
2025-06-12 19:11:35 +0530

    Update MayorElection.java

--- src/main/java/com/mayorelection/MayorElection.java
+++ src/main/java/com/mayorelection/MayorElection.java
@@ -1,10 +1,10 @@
 package com.mayorelection;
 
+import com.mayorelection.models.Mayor;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.PluginCommand;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
