3b12b120e2bb9f3780e1d8eb657d56208f1945ef
Md Mahfuz Rahman <tanzimrani8@gmail.com>
2025-06-12 19:17:34 +0530

    Update Mayor.java

--- src/main/java/com/mayorelection/models/Mayor.java
+++ src/main/java/com/mayorelection/models/Mayor.java
@@ -1,14 +1,54 @@
-private String perk;
+package com.mayorelection.models;
 
-public Mayor(String name) {
-    this.name = name;
-    this.perk = "Default Perk"; // or whatever logic
-}
+public class Mayor {
 
-public String getPerk() {
-    return perk;
-}
+    public enum Perk {
+        DOUBLE_MINING_XP,
+        CHEAPER_SHOPS,
+        EXTRA_MOB_DROPS,
+        FASTER_CROPS,
+        SERVER_BOOST
+    }
 
-public void setPerk(String perk) {
-    this.perk = perk;
+    private final String name;
+    private Perk perk;
+
+    public Mayor(String name) {
+        this.name = name;
+        this.perk = Perk.DOUBLE_MINING_XP; // default perk
+    }
+
+    public Mayor(String name, Perk perk) {
+        this.name = name;
+        this.perk = perk;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Perk getPerk() {
+        return perk;
+    }
+
+    public void setPerk(Perk perk) {
+        this.perk = perk;
+    }
+
+    public String getFormattedPerk() {
+        switch (perk) {
+            case DOUBLE_MINING_XP:
+                return "Double Mining XP";
+            case CHEAPER_SHOPS:
+                return "Cheaper Shops";
+            case EXTRA_MOB_DROPS:
+                return "Extra Mob Drops";
+            case FASTER_CROPS:
+                return "Faster Crops";
+            case SERVER_BOOST:
+                return "Server Boost";
+            default:
+                return "Unknown Perk";
+        }
+    }
 }
